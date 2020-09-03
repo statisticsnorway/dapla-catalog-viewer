961de5e59dfb5a558520f19616d0aea5136b7119
mmj <mmj@ssb.no>
2020-09-03 15:33:27 +0200

    Integrated more from dapla-js-utilities, upped test coverage and updated dependencies which does not affect the published package, therefore there will be no new release

--- src/configurations/TEST.js
+++ src/configurations/TEST.js
@@ -1,16 +1,13 @@
 import { LANGUAGE } from '@statisticsnorway/dapla-js-utilities'
 
-import TestCatalogs from '../__tests__/test-data/TestCatalogs.json'
-
 export const TEST_CONFIGURATIONS = {
   alternativeApi: 'http://localhost:9999',
   apiContext: (fn) => ({
     api: process.env.REACT_APP_API,
     setApi: fn
   }),
-  catalogs: TestCatalogs,
-  emptyCatalogs: { catalogs: [] },
   errorString: 'A problem occured',
   language: LANGUAGE.LANGUAGES.NORWEGIAN.languageCode,
-  otherLanguage: LANGUAGE.LANGUAGES.ENGLISH.languageCode
+  otherLanguage: LANGUAGE.LANGUAGES.ENGLISH.languageCode,
+  testPath: '/test/path/1'
 }
