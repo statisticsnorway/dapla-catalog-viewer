090406ecea61a4eb9a5a24ee8d03e22a7c44e809
Magnus <32482475+mmj-ssb@users.noreply.github.com>
2021-01-05 12:29:49 +0100

    Update (#13)
    Dependencies

--- src/utilities/StringHandling.js
+++ src/utilities/StringHandling.js
@@ -1,7 +1,7 @@
 export const convertDateToView = (value, seconds = false) => {
   if (value !== undefined) {
     const date = new Date(value - 1000)
-    let options = { hour: '2-digit', minute: '2-digit' }
+    const options = { hour: '2-digit', minute: '2-digit' }
 
     if (seconds) {
       options.second = '2-digit'
